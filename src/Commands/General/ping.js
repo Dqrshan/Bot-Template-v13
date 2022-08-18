c03f46584a1dba6d72526eea1a5dc0e3c725167f
Nin <darshan310268@gmail.com>
2022-08-18 17:53:19 +0530

    feat: EmbedBuilder() usage

--- src/Commands/General/ping.js
+++ src/Commands/General/ping.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require('discord.js')
 const Command = require('../../Client/Command')
 
 module.exports = new Command({
@@ -6,9 +7,17 @@ module.exports = new Command({
     description: 'The ping of the bot',
     category: 'General',
     run: async (client, message, args) => {
-        message.reply(`Pong!・\`${client.ws.ping}\` ms.`).catch(() => {})
+        message
+            .reply({
+                embeds: [new EmbedBuilder().setDescription(`Pong!・\`${client.ws.ping}\` ms.`)],
+            })
+            .catch(() => {})
     },
     exec: async (client, interaction) => {
-        interaction.reply(`Pong!・\`${client.ws.ping}\` ms.`).catch(() => {})
+        interaction
+            .reply({
+                embeds: [new EmbedBuilder().setDescription(`Pong!・\`${client.ws.ping}\` ms.`)],
+            })
+            .catch(() => {})
     },
 })
